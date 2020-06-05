68e0da23ec5140530024a3347f5991d27e4f25ed
tuanpnmfx00196 <phantuanqb85@gmail.com>
2020-06-06 06:53:15 +0700

    UI dashboard, stuck edittext in recyclerview

--- app/src/main/java/com/example/actproperty/DashBoard.java
+++ app/src/main/java/com/example/actproperty/DashBoard.java
@@ -1,21 +1,43 @@
 package com.example.actproperty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class DashBoard extends AppCompatActivity {
+    Button btnCableId;
+    ImageButton btnImgCableId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
-
+        btnCableId = (Button)findViewById(R.id.btnCableId);
+        btnImgCableId = (ImageButton)findViewById(R.id.imgBtnCableId);
+        btnCableId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toManagement();
+            }
+        });
+        btnImgCableId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toManagement();
+            }
+        });
+    }
+    public void toManagement(){
+        Intent intent = new Intent(DashBoard.this, Management.class);
+        startActivity(intent);
     }
 
 }
