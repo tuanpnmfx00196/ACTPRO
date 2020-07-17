f6658db661fc062fc529ad184a054b688accd788
Phan Tuan <phantuanqb85@gmail.com>
2020-07-17 17:28:56 +0700

    compare time

--- app/src/main/java/com/example/actproperty/department/accounting/AccountingDepartment.java
+++ app/src/main/java/com/example/actproperty/department/accounting/AccountingDepartment.java
@@ -14,28 +14,43 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.actproperty.R;
 import com.example.actproperty.passport.Passport;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class AccountingDepartment extends AppCompatActivity {
 ImageButton imgBtnIO, imgBtnShowInventory, imgBtnDeliver;
-EditText getFromDateIO, getToDateIO;
+TextView getFromDateIO, getToDateIO;
 Spinner spinnerCodeStore;
 ArrayList<Passport>listUser;
+ArrayList<DeliverObject>listDeliver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounting_department);
         listUser = new ArrayList<>();
+        listDeliver = new ArrayList<>();
         getAccount();
         Map();
+        GetDataDeliver("https://sqlandroid2812.000webhostapp.com/gettempdeliver.php");
         imgBtnIO.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,8 +81,8 @@ ArrayList<Passport>listUser;
         dialog.setContentView(R.layout.dialog_ioinventory);
         int width = (int)(getResources().getDisplayMetrics().widthPixels*0.97);
         dialog.getWindow().setLayout(width, LinearLayout.LayoutParams.WRAP_CONTENT);
-        getFromDateIO = (EditText)dialog.findViewById(R.id.getFromDateIO);
-        getToDateIO = (EditText)dialog.findViewById(R.id.getToDateIO);
+        getFromDateIO = (TextView)dialog.findViewById(R.id.getFromDateIO);
+        getToDateIO = (TextView)dialog.findViewById(R.id.getToDateIO);
         Button btnSearchIO = (Button)dialog.findViewById(R.id.btnSearchIO);
         Button btnBackIO = (Button)dialog.findViewById(R.id.btnBackIO);
         spinnerCodeStore = (Spinner)dialog.findViewById(R.id.spinnerCodeStore);
@@ -98,15 +113,15 @@ ArrayList<Passport>listUser;
 
             }
         });
-        getFromDateIO.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        getFromDateIO.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
+            public void onClick(View v) {
                 GetFromDate();
             }
         });
-        getToDateIO.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        getToDateIO.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
+            public void onClick(View v) {
                 GetToDate();
             }
         });
@@ -148,6 +163,65 @@ ArrayList<Passport>listUser;
             }
         },_year, _month, _date);
         datePickerDialog.show();
+    }
+    private void GetDataDeliver(String url){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i=0;i<response.length();i++){
+                    try{
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        listDeliver.add(new DeliverObject(
+                                jsonObject.getInt("IDdeliver"),
+                                jsonObject.getString("Storecodedeliver"),
+                                jsonObject.getInt("Hanging6fodeliver"),
+                                jsonObject.getInt("Hanging12fodeliver"),
+                                jsonObject.getInt("Hanging24fodeliver"),
+                                jsonObject.getInt("Odf6fodeliver"),
+                                jsonObject.getInt("Odf12fodeliver"),
+                                jsonObject.getInt("Odf24fodeliver"),
+                                jsonObject.getInt("Closure6fodeliver"),
+                                jsonObject.getInt("Closure12fodeliver"),
+                                jsonObject.getInt("Closure24fodeliver"),
+                                jsonObject.getInt("Buloongti300deliver"),
+                                jsonObject.getInt("Buloongti400deliver"),
+                                jsonObject.getInt("Clampdeliver"),
+                                jsonObject.getInt("Sc_lc5deliver"),
+                                jsonObject.getInt("Sc_lc10deliver"),
+                                jsonObject.getString("Userdeliver"),
+                                jsonObject.getString("Timedeliver"),
+                                jsonObject.getString("Commentdeliver"),
+                                jsonObject.getInt("Flag")
+                        ));
+                    }catch(Exception e){
+                        Toast.makeText(AccountingDepartment.this, e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(AccountingDepartment.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        requestQueue.add(request);
+    }
+    private ArrayList<DeliverObject> ExportDeliver(String fromDate, String toDate, String codeStore,
+                                                   ArrayList<DeliverObject> arrayList){
+        ArrayList<DeliverObject> listDeliverSearch = new ArrayList<>();
+        try {
+            Date from = new SimpleDateFormat("dd/MM/yyyy").parse(fromDate);
+        } catch (ParseException e) {
+            Toast.makeText(this, "Lỗi ngày bắt đầu", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            Date to = new SimpleDateFormat("dd/MM/yyyy").parse(toDate);
+        } catch (ParseException e) {
+            Toast.makeText(this, "Lỗi ngày kết thúc", Toast.LENGTH_SHORT).show();
+        }
+        return listDeliverSearch;
     }
 
 
